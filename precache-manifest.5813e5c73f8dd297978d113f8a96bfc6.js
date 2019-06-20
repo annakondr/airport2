39dadaf346e1d5d16d69b0b33966bd368801c090
annakondr <tyukondra@gmail.com>
2019-06-20 21:05:37 +0300

    Updates

---
+++ precache-manifest.5813e5c73f8dd297978d113f8a96bfc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2612403d254aa67414797912d39b8a8",
+    "url": "/airport2/index.html"
+  },
+  {
+    "revision": "e011fc27296cb6784844",
+    "url": "/airport2/static/css/main.42ad8071.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/airport2/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "e011fc27296cb6784844",
+    "url": "/airport2/static/js/main.6e4407d2.chunk.js"
+  },
+  {
+    "revision": "d2e6b87f2776b11aecd7",
+    "url": "/airport2/static/js/runtime~main.75466b72.js"
+  }
+]);
